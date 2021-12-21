1907993051d62a51fedcfa0c7fde9cec6fb4aac7
angxuejian <xuejian.ang@gmail.com>
2021-12-21 18:10:26 +0800

    feat: add html template

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ const _FUNCTIONS = { ...formatString, ...types }
 
 for (const key in _FUNCTIONS) window[key] = _FUNCTIONS[key]
 window.Moate = Moate
+
+// -------------------
+// 测试是否已挂载到页面上
+console.log(window, '--->')
+const moate = new Moate()
+console.log('今年第' + moate.getWeekNumber() + '周')
+console.log('类型是否为string: ', isTypeOf([], 'string'))
+console.log('moScrollbar: ', toUpperCaseH('moScrollbar'))
