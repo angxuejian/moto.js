77e30d254b31d7eeea7707844100d0c1aedab2bd
angxuejian <xuejian.ang@gmail.com>
2021-12-25 15:48:00 +0800

    chore: replace compression plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
+const TerserJsPlugin = require("terser-webpack-plugin")
 module.exports = {
+  // mode: 'production',
   mode: 'development',
   entry: ['./src/index.js'],
 
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Moto.js - sdk',
-    })
+    }),
+    
   ],
   module: {
     rules: [
@@ -25,7 +26,20 @@ module.exports = {
   },
 
   optimization: {
-    minimizer: [new UglifyJsPlugin()]
+    minimize: true, // 只在 production 下压缩js
+    minimizer: [
+      new TerserJsPlugin({
+        extractComments: false, // index.js.LICENSE.txt文档
+        terserOptions: {
+          compress: {
+            warnings: false,
+            drop_console: true,
+            drop_debugger: true,
+            pure_funcs: ['console.log']
+          }
+        }
+      })
+    ]
   },
   output: {
     filename: 'index.js',
